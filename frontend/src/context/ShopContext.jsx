d2474d49f37c4a3d6ec7aa112776f2bcc68f5372
Uzairjameel <uzairjameel248@gmail.com>
2025-10-11 17:49:41 +0100

    Update ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useCallback, useContext, useEffect, useState } from "react";
-import { authDataContext } from "./authcontext";
+import { authDataContext } from "./Authcontext";
 import axios from 'axios'
 import { UserDataContext } from "./UserContext";
 
