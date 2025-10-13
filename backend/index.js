ee49f0ff531cd3a3784f61e8b4c15c589b5118c0
Uzairjameel <uzairjameel248@gmail.com>
2025-10-13 23:41:36 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ let app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin:['http://localhost:5173','http://localhost:5174'],
+    origin:['https://pakistanmart.netlify.app','http://localhost:5174'],
     credentials:true
 }))
 app.use(cookieParser())
